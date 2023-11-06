e2ebcde4d0334e48bbc6c3acec34356efefabb42
Alexandermis <73187089+Alexandermis@users.noreply.github.com>
2023-11-07 00:30:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,18 +13,20 @@ cp .tmux.conf ~/.tmux.conf
 source ~/.tmux.conf
 echo ".vimrc copied"
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Maybe this is a little bit buggy
 mkdir -p /home/mrx/Repos
 # Clone the first repository
 REPO_URL1="https://github.com/marlonrichert/zsh-autocomplete.git"
 TARGET_DIR1="/home/mrx/Repos/zsh-autocomplete"
 git clone "$REPO_URL1" "$TARGET_DIR1"
-
+# Maybe this is a little bit buggy
 # Clone the second repository
 REPO_URL2="https://github.com/zsh-users/zsh-syntax-highlighting.git"
 TARGET_DIR2="/home/mrx/Repos/zsh-syntax-highlighting"
 git clone "$REPO_URL2" "$TARGET_DIR2"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 cp .zshrc ~/.zshrc
 source ~/.zshrc
