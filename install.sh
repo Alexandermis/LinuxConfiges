cb06db1b6cfb508bbe7815a3c61cb52f95722670
Alexandermis <73187089+Alexandermis@users.noreply.github.com>
2024-03-07 21:09:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -52,3 +52,4 @@ sudo chsh -s $(which zsh) $USER
 reboot now
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 # https://github.com/romkatv/powerlevel10k#is-it-really-fast
+history -p
