00616114de2b515d92c70d8eebef16f8100bf95f
Alexandermis <73187089+Alexandermis@users.noreply.github.com>
2024-03-07 19:16:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,17 @@ if [ -e /etc/os-release ]; then
     echo "Operating System: $os_name"
 fi
 # oh my gosh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # gdb gef
 # bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
 #update and install
 sudo apt update
 sudo apt install -y tmux vim zsh git bat
+#fix bat
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
+#install lsd
+snap install lsd
 #vimrc
 cp .vimrc ~/.vimrc
 source ~/.vimrc
@@ -23,14 +28,14 @@ echo ".vimrc copied"
 # zsh autosuggetstions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # zsh autocomplete
-mkdir -p /home/mrx/Repot
+mkdir ~/Repos
 # Clone the first repository
 REPO_URL1="https://github.com/marlonrichert/zsh-autocomplete.git"
-TARGET_DIR1="/home/mrx/Repos"
-git clone "$REPO_URL1" "$TARGET_DIR1"
+TARGET_DIR1="~/Repos"
+sudo git clone "$REPO_URL1" "$TARGET_DIR1"
 # zsh syntax highlighting
 REPO_URL2="https://github.com/zsh-users/zsh-syntax-highlighting.git"
-TARGET_DIR2="/home/mrx/Repos"
+TARGET_DIR1="~/Repos"
 git clone "$REPO_URL2" "$TARGET_DIR2"
 echo "ZSH Syntax Hightling done"
 #zshrc config
