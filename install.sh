eb405021eb3d041f047704a0d62cdcf277ef968d
Alexandermis <73187089+Alexandermis@users.noreply.github.com>
2024-03-07 19:32:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,20 +5,21 @@ if [ -e /etc/os-release ]; then
     os_name=$(cat /etc/os-release | grep -oP '(?<=PRETTY_NAME=")([^"]+)')
     echo "Operating System: $os_name"
 fi
-#update and install
+# update and install
 sudo apt update
+# for virtualbox
+#sudo apt install build-essential dkms linux-headers-$(uname -r)
+# and Guest Additions
 sudo apt install -y tmux vim zsh git bat
+# fix bat
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
-#install lsd
+# install lsd
 snap install lsd
-# for virtualbox
-# sudo apt install build-essential dkms linux-headers-$(uname -r)
 # oh my gosh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # gdb gef
 # bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
-#fix bat
 #vimrc
 cp .vimrc ~/.vimrc
 source ~/.vimrc
@@ -32,14 +33,10 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # zsh autocomplete
 mkdir ~/Repos
 # Clone the first repository
-REPO_URL1="https://github.com/marlonrichert/zsh-autocomplete.git"
-TARGET_DIR1="~/Repos"
-sudo git clone "$REPO_URL1" "$TARGET_DIR1"
+sudo git clone https://github.com/marlonrichert/zsh-autocomplete.git ~/Repos
 # zsh syntax highlighting
-REPO_URL2="https://github.com/zsh-users/zsh-syntax-highlighting.git"
-TARGET_DIR1="~/Repos"
-git clone "$REPO_URL2" "$TARGET_DIR2"
-echo "ZSH Syntax Hightling done"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/Repos
+echo "ZSH Syntax highlighting done"
 #zshrc config
 cp .zshrc ~/.zshrc
 git clone "https://github.com/z-shell/zsh-lsd.git" "~/.oh-my-zsh/custom/plugins"
