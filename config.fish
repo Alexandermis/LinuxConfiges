d63cceec69ec654ee3decccd09c13ea6a3dcdf1d
Alexandermis <73187089+Alexandermis@users.noreply.github.com>
2025-04-05 11:17:43 +0000

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,10 +1,12 @@
 if status is-interactive
+    # Vim
     fish_vi_key_bindings
     set -Ux EDITOR vim
+
+    # Code color
     set -g fish_color_error red
     set -g fish_color_command green
 
     # Alias
     alias s='ssh'
-    # Commands to run in interactive sessions can go here
 end
